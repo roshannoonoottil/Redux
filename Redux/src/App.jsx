9d17f948d610f037751ad9f08d93cc962650b18e
roshannoonoottil <roshannoonoottil@gmail.com>
2024-10-29 16:54:41 +0530

    1st commit

---
+++ Redux/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Redux Counter</h1>
+    <div>
+        <button id="decrement">Decrement</button>
+        <span id="value">0</span>
+        <button id="increment">Incerement</button>
+    </div>
+    </>
+  )
+}
+
+export default App
