4018b5466acf6816a088f309b003214077dd7ff0
roshannoonoottil <roshannoonoottil@gmail.com>
2024-11-04 16:19:47 +0530

    Revert "Immer Function and removed strictmode"
    This reverts commit 3474687f01c55ee26db4c48bb4013b296c90b247.

--- Redux/src/assets/Pages/ImmerLibrary.jsx
+++ Redux/src/assets/Pages/ImmerLibrary.jsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useState } from 'react'
-import { produce } from 'immer';
 
 
 const initialState = [
@@ -58,26 +57,20 @@ function ImmerLibrary() {
 
         setInfo((state) => {
 
-            //Insted of shallow clonig and deep cloning we can use immer function
-
-            const newState = produce(state, (draft)=>{
-                draft[0].rooms[0].text = evt.target.value;
-            })
-
             //Shallow Cloning
 
-            // var newState = [
-            //     {
-            //         ...state[0],
-            //         rooms :[
-            //         {
-            //             ...state[0].rooms[0], 
-            //             text : evt.target.value
-            //         }
-            //     ]
-            //     },
-            //     state[1]
-            // ];
+            var newState = [
+                {
+                    ...state[0],
+                    rooms :[
+                    {
+                        ...state[0].rooms[0], 
+                        text : evt.target.value
+                    }
+                ]
+                },
+                state[1]
+            ];
 
 
             //Deep Cloning
@@ -90,20 +83,15 @@ function ImmerLibrary() {
     const onCarChange = useCallback((evt) =>{
         setInfo((state) => {
 
-            //Same here Insted of shallow clonig and deep cloning we can use immer function
-            const newState = produce(state, (draft)=>{
-                draft[1].text = evt.target.value;
-            })
-
             //Shallow Cloning
 
-            // var newState = [
-            //     state[0],
-            //     {
-            //         ...state[1],
-            //         text : evt.target.value
-            //     }
-            // ];
+            var newState = [
+                state[0],
+                {
+                    ...state[1],
+                    text : evt.target.value
+                }
+            ];
 
 
             //Deep Cloning
